01b2ebb0afe9351da48d73530654228a96a3bb8d
traditionalnaturals-max <traditionalnaturals@gmail.com>
2026-07-04 21:19:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("TN Naturals AI Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
